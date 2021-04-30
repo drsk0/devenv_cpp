238459a0c219a9bfc293e5fe5b5d2cd67d3c120c
drsk <drsk@lavabit.com>
2021-04-30 22:27:46 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "C++ development environment";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config = { allowUnfree = true; allowBroken = true; };
+        # overlays = [ self.overlay ];
+      };
+    in
+    {
+
+      devShell.${system} = pkgs.mkShell.override ({ stdenv = pkgs.llvmPackages.stdenv; }) rec {
+        buildInputs = [
+          pkgs.clang
+          pkgs.llvmPackages.libclang
+          pkgs.cmake
+          pkgs.ninja
+        ];
+        CPATH = pkgs.lib.makeSearchPathOutput "dev" "include" buildInputs;
+        shellHook = ''
+          export CPATH=${CPATH}
+          export C_INCLUDE_PATH=${CPATH}
+        '';
+      };
+    };
+}
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <iostream>
+
+int main ()
+{
+  std::cout << "Type your name" << std::endl;
+  std::string name;
+  std::cin >> name;
+  std::cout << "Hello, " << name << std::endl;
+
+  return 0;
+}
+
